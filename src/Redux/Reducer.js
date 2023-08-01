e3a21b943380f5bc51b26cddaaa4aa127de28664
Yiğit Korkmaz <102830923+BenYiit10@users.noreply.github.com>
2023-08-01 15:27:58 +0300

    zincirleme işlemler bitti

--- src/Redux/Reducer.js
+++ src/Redux/Reducer.js
@@ -10,6 +10,7 @@ export const processes = createSlice({
         symbol:"",
         resultAppreance:[],
         clickable:true,
+        clickAgain:false,
 
         chainingEqual:[],
         total:0,
@@ -24,9 +25,16 @@ export const processes = createSlice({
             
             state.resultAppreance=[...state.resultAppreance,action.payload]
             
-            
             state.clickable=true;
+
+            if (state.clickAgain===true) {
+                state.chainingEqual.splice(0,1)
+                console.log(state.chainingEqual)
+            }
+            state.clickAgain=false
         },
+
+
         symbol:(state,action) => {
             if(state.clickable===true) {
                 state.prevNum=parseInt(state.num)
@@ -34,34 +42,74 @@ export const processes = createSlice({
                 state.resultAppreance=[...state.resultAppreance,state.symbol]
                 
                 state.chainingEqual=[...state.chainingEqual,state.presentNum,state?.symbol]
-                console.log(state.chainingEqual/* .reduce(function (param) {  }) */)
+                console.log(state.chainingEqual)
                 
                 state.num="0"
                 state.clickable=false
-            }
-        },
-        equal:(state) => {
-            try {
-                state.num= eval(state.prevNum+state.symbol+parseFloat(state.presentNum))
-                state.total = parseFloat(state.num)
-                state.resultAppreance=[state.num]
 
-                state.chainingEqual=[...state.chainingEqual,state.presentNum]
-                console.log(state.chainingEqual/* .reduce(function (param) {  }) */)
-                
-                console.log(state.chainingEqual.pop())
-                if(state.chainingEqual.pop()=== "+"||"-"||"/"||"*") {
-                    console.log("bu iş yürümze")
+                if (typeof state.chainingEqual[0] == "number" && typeof state.chainingEqual[1] == "number") {
+                    state.chainingEqual.splice(1,1)
                 }
-                else {
-                    console.log("ya allah")
-                }
-            }
-            catch (error) {
-                console.log(error)
-                state.num=0
+                state.clickAgain=false
             }
         },
+
+
+        equal:(state) => {
+            if (state.clickable===true) {
+                try {
+                    
+                    // eslint-disable-next-line
+                    state.num= eval(state.prevNum+state.symbol+parseFloat(state.presentNum))
+                    state.total = parseFloat(state.num)
+                    state.resultAppreance=[state.num]
+    
+                    state.chainingEqual=[...state.chainingEqual,state.presentNum]
+                    console.log(state.chainingEqual)
+                    
+                    let stringForEval = ""
+                    state.chainingEqual.forEach((item) => {
+                        stringForEval+=item
+                    })
+
+                    // eslint-disable-next-line
+                    state.num=eval(stringForEval)
+                    state.resultAppreance=[state.num]
+                }
+                catch (error) {
+                    console.log(error)
+                    state.num=0
+                }
+            }
+            else {
+                try {
+                    // eslint-disable-next-line
+                    state.num= eval(state.prevNum+state.symbol+parseFloat(state.presentNum))
+                    state.total = parseFloat(state.num)
+                    state.resultAppreance=[state.num]
+    
+                    state.chainingEqual=[...state.chainingEqual,state.presentNum]
+                    
+                    let stringForEval = ""
+                    let chainingEqualSliced = state.chainingEqual.slice(0, -1)
+                    chainingEqualSliced.forEach((item) => {
+                        stringForEval+=item
+                    })
+
+                    // eslint-disable-next-line
+                    state.num=eval(stringForEval)
+                    state.resultAppreance=[state.num]
+                }
+                catch (error) {
+                    console.log(error)
+                    state.num=0
+                }
+            }
+            state.chainingEqual=[state.num]
+            state.clickAgain=true
+        },
+
+
         restart:(state) => {
             state.num="0"
             state.prevNum=0
@@ -69,6 +117,7 @@ export const processes = createSlice({
             state.symbol=""
             state.total=0
             state.resultAppreance=[]
+            state.chainingEqual=[]
         }
     },
 },
